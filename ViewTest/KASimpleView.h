50cb685dd1b8427327e35df66d46dd764a6ddfee
Kate Aplin <kaplin@hmc.edu>
2014-09-10 19:15:46 -0700

    Gitignore, and through ViewTest

---
+++ ViewTest/KASimpleView.h
@@ -0,0 +1,13 @@
+//
+//  KASimpleView.h
+//  ViewTest
+//
+//  Created by Kathryn Aplin on 9/10/14.
+//  Copyright (c) 2014 Kathryn Aplin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KASimpleView : UIView
+
+@end
---
+++ ViewTest/KASimpleView.m
@@ -0,0 +1,13 @@
+//
+//  KASimpleView.m
+//  ViewTest
+//
+//  Created by Kathryn Aplin on 9/10/14.
+//  Copyright (c) 2014 Kathryn Aplin. All rights reserved.
+//
+
+#import "KASimpleView.h"
+
+@implementation KASimpleView
+
+@end
